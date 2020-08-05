45c7e832192c820257594d40e7779b333910e354
Carlos Kauan <carloskauanmoreiradesousa@gmail.com>
2020-08-05 02:30:31 -0300

    Starting to implement Dark Mode /1

--- scripts/brightness-mode.js
+++ scripts/brightness-mode.js
@@ -1,6 +1,27 @@
-let button = document.getElementById('float-button');
+// pegamos o valor no localStorage
+const nightModeStorage = localStorage.getItem('gmtNightMode')
+const nightMode = document.querySelector('#night-mode')
 
-button.addEventListener('click', () => {
-    button.classList.toggle('light');
+// caso tenha o valor no localStorage
+if (nightModeStorage) {
+  // ativa o night mode
+  document.documentElement.classList.add('dark')
+
+  // já deixa o input marcado como ativo
+  nightMode.checked = true
+}
+
+// ao clicar mudaremos as cores
+nightMode.addEventListener('click', () => {
+  // adiciona a classe `dark` ao html
+  document.documentElement.classList.toggle('dark')
+
+  // se tiver a classe dark
+  if (document.documentElement.classList.contains('dark')) {
+    // salva o tema no localStorage
+    localStorage.setItem('gmtNightMode', true)
+    return
+  }
+  // senão remove
+  localStorage.removeItem('gmtNightMode')
 })
-
